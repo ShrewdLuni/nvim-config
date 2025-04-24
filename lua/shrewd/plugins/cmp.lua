18e11351173537170fb7ba533baf9e88d24f7472
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2025-04-24 15:14:07 +0200

    Styled CMP

--- lua/shrewd/plugins/cmp.lua
+++ lua/shrewd/plugins/cmp.lua
@@ -3,6 +3,8 @@ return {
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
   },
+  event = { "BufReadPre", "BufNewFile" },
+  priority = 1001,
   config = function()
     local cmp = require('cmp')
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -13,22 +15,76 @@ return {
        require("cmp_nvim_lsp").default_capabilities()
      )
 
+    local icons = {
+        Text = "",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "󰇽",
+        Variable = "󰂡",
+        Class = "󰠱",
+        Interface = "",
+        Module = "",
+        Property = "󰜢",
+        Unit = "",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "󰈇",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "󰙅",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "󰅲",
+    }
+
     cmp.setup({
       sources = {
         {name = 'nvim_lsp'},
       },
       window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered({
+            scrollbar = false,
+            winhighlight = "Normal:CmpDocsNormal,FloatBorder:CmpFloatBorder,CursorLine:CmpCursorLine,Search:CmpSearch",
+        }),
+        completion = cmp.config.window.bordered({
+            scrollbar = false,
+            winhighlight = "Normal:CmpNormal,FloatBorder:CmpFloatBorder,CursorLine:CmpCursorLine,Search:CmpSearch",
+        }),
       },
       formatting = {
-        format = require("nvim-highlight-colors").format
+          format = function(entry, item)
+              require("cmp-tailwind-colors").format(entry, item)
+              item.kind = string.format("%s %s", icons[item.kind] or "", item.kind)
+
+              local function trim(text)
+                local max = 20
+                if text and text:len() > max then
+                    text = text:sub(1, max) .. "..."
+                end
+                return text
+              end
+
+              item.abbr = trim(item.abbr)
+
+              item.menu = nil
+
+              return item
+          end,
       },
       mapping = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
       }),
     })
   end,
--- lua/shrewd/plugins/lsp.lua
+++ lua/shrewd/plugins/lsp.lua
@@ -1,29 +1,37 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-nvim-lsp',
-    'folke/lsp-colors.nvim',
-  },
-  config = function()
-    require('lspconfig').lua_ls.setup({
-    settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      },
-    })
-    require('lspconfig').pyright.setup({disableOrganizeImports = true})
-    require('lspconfig').ts_ls.setup({capabilities = Capabilities})
-    require('lspconfig').omnisharp.setup({})
-    require('lspconfig').csharp_ls.setup({})
-    require('lspconfig').clangd.setup({})
-    require('lspconfig').tailwindcss.setup({capabilities = Capabilities})
-    require('lspconfig').cssls.setup({capabilities = Capabilities})
-    require('lspconfig').emmet_language_server.setup({capabilities = Capabilities})
-  end,
+    'neovim/nvim-lspconfig',
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'folke/lsp-colors.nvim',
+    },
+    config = function()
+        local original_notify = vim.notify
+        vim.notify = function(msg, ...)
+            if type(msg) == "string" and msg:match("csharp%-ls is released under MIT license") then
+                return
+            end
+            return original_notify(msg, ...)
+        end
+        require('lspconfig').lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+        require('lspconfig').pyright.setup({ disableOrganizeImports = true, capabilities = Capabilities})
+        require('lspconfig').ts_ls.setup({ capabilities = Capabilities })
+        -- require('lspconfig').omnisharp.setup({ cmd = { "D:/Software/NvimOmnisharp/OmniSharp.exe", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) } })
+        require('lspconfig').csharp_ls.setup({ capabilities = Capabilities})
+        require('lspconfig').clangd.setup({})
+        require('lspconfig').tailwindcss.setup({ capabilities = Capabilities })
+        require('lspconfig').cssls.setup({ capabilities = Capabilities })
+        require('lspconfig').emmet_language_server.setup({ capabilities = Capabilities })
+    end,
 }
